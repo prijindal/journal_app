5942fce571a05f3cb4a4369d0847b7963c489612
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2024-12-19 13:50:45 +0530

    Fixed tests
    Added mock router

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,16 +1,47 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:journal_app/app/app.dart';
+import 'package:journal_app/models/settings.dart';
+import 'package:journal_app/pages/home/index.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+
+class MockStackRouter extends Mock implements StackRouter {}
+
+class FakePageRouteInfo extends Fake implements PageRouteInfo {}
 
 void main() {
+  setUpAll(() {
+    registerFallbackValue(FakePageRouteInfo());
+  });
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
     testWidgets('tap on the floating action button, verify counter',
         (tester) async {
+      final mockStackRouter = MockStackRouter();
+      when(() => mockStackRouter.pushNamed(any())).thenAnswer((_) async {
+        return null;
+      });
       // Load app widget.
-      await tester.pumpWidget(const MyApp());
+      await tester.pumpWidget(
+        ChangeNotifierProvider<SettingsStorageNotifier>(
+          child: MaterialApp(
+            title: 'Routing Test',
+            home: StackRouterScope(
+              controller: mockStackRouter,
+              stateHash: 0,
+              child: HomeScreen(),
+            ),
+          ),
+          create: (context) => SettingsStorageNotifier(
+            ThemeMode.system,
+            ColorSeed.baseColor,
+            HiddenLockedMode.unknown,
+          ),
+        ),
+      );
 
       // Verify the counter starts at 0.
       expect(find.text('Journals'), findsOneWidget);
